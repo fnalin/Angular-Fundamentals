ca89d5492d4186791727ec10e6f22a91d9418734
Fabiano Nalin <fnalin@msn.com>
2015-12-25 23:09:15 -0200

    Finally 02.01 - Demo Controllers

---
+++ 02. Controllers & Markup/02.01 - Demo Controllers/DemoApp/DemoApp.UI/app/controllers/EventController.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+
+        //representa um evento
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '1/1/2013',
+            time: '10:30 am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Montain View',
+                province: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png'
+
+        };
+    }
+);
