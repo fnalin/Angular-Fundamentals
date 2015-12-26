e516a1d324ceff8f56d2f88e5cced74815913e7b
Fabiano Nalin <fnalin@msn.com>
2015-12-26 16:08:11 -0200

    02.02 - Validation

--- 02. Controllers & Markup/02.01 - Demo Controllers/DemoApp/DemoApp.UI/app/js/controllers/EditEventController.js
+++ 02. Controllers & Markup/02.01 - Demo Controllers/DemoApp/DemoApp.UI/app/js/controllers/EditEventController.js
@@ -1,8 +1,10 @@
 ﻿'use strict'
 
 eventsApp.controller('EditEventController', function ($scope) {
-    $scope.saveEvent = function (event) {
-        window.alert('event: ' + event.name + ' saved!');
+    $scope.saveEvent = function (event, newEventForm) {
+        if (newEventForm.$valid) {
+            window.alert('event: ' + event.name + ' saved!');
+        }
     };
 
     $scope.cancelEdit = function () {
