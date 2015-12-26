ef2e1983e290e86ece8b261f82dedae5185eab34
Fabiano Nalin <fnalin@msn.com>
2015-12-26 21:04:25 -0200

    03. Add WebApi project

---
+++ 03. Services/DemoApp/DemoApp.Api/Controllers/EventsController.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Web.Http;
+
+namespace DemoApp.Api.Controllers
+{
+    public class EventsController : ApiController
+    {
+        public JToken Get(string id = null)
+        {
+            var dados = JObject.Parse(File.ReadAllText(path + id + ".json"));
+            return dados;
+        }
+
+        public void Post(string id, JObject eventData)
+        {
+            File.WriteAllText(path + id + ".json",eventData.ToString(Formatting.None));
+        }
+
+        private string path
+        {
+            get
+            {
+                var host = System.Web.Hosting.HostingEnvironment.MapPath("/");
+                var _path = host + "../DemoApp.UI/app/data/event/";
+                return _path;
+            }
+        }
+    }
+}
--- 03. Services/DemoApp/DemoApp.UI/app/js/services/EventData.js
+++ 03. Services/DemoApp/DemoApp.UI/app/js/services/EventData.js
@@ -7,7 +7,7 @@
 eventsApp.factory('eventData', function ($http,$log) {
     return {
         getEvent: function (successCb) {
-            $http({ method: 'GET', url: '/data/event/1' })
+            $http({ method: 'GET', url: 'http://localhost:62502/api/events/1' })
                 .success(function (data, status, headers, config) {
                     successCb(data);
                 })
