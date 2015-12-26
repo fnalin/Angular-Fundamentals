7a14206e4559e39af579d26a2322ff6b3690e164
Fabiano Nalin <fnalin@msn.com>
2015-12-26 15:10:38 -0200

    02.02 - Demo - Two Way Binding

---
+++ 02. Controllers & Markup/02.01 - Demo Controllers/DemoApp/DemoApp.UI/app/js/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="app.js" />
+/// <reference path="filters.js" />
+/// <reference path="controllers/EventController.js" />
+/// <reference path="controllers/EditEventController.js" />
---
+++ 02. Controllers & Markup/02.01 - Demo Controllers/DemoApp/DemoApp.UI/app/js/controllers/EditEventController.js
@@ -0,0 +1,11 @@
+﻿'use strict'
+
+eventsApp.controller('EditEventController', function ($scope) {
+    $scope.saveEvent = function (event) {
+        window.alert('event: ' + event.name + ' saved!');
+    };
+
+    $scope.cancelEdit = function () {
+        window.location = "/app/EventDetails.html";
+    };
+});
